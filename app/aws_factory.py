1a7ac516cc2a538f7354f5c0fb7eeb577e9115aa
Donald Gray <donald.gray@digirati.com>
2022-01-17 10:30:33 +0000

    Tidy readme, add sample.json

--- app/aws_factory.py
+++ app/aws_factory.py
@@ -8,7 +8,12 @@ def get_aws_client(client_type: str):
     """Get an aws client configured to use LocalStack if env var is set"""
     if LOCALSTACK:
         logger.warn(f"Using localstack for {client_type} client")
-        return boto3.client(client_type, region_name=REGION, endpoint_url=LOCALSTACK_ADDRESS)
+        return boto3.client(client_type,
+                            region_name=REGION,
+                            endpoint_url=LOCALSTACK_ADDRESS,
+                            aws_access_key_id='foo',
+                            aws_secret_access_key='bar',
+                            )
     else:
         return boto3.client(client_type, REGION)
 
@@ -17,6 +22,11 @@ def get_aws_resource(resource_type: str):
     """Get an aws resource configured to use LocalStack if env var is set"""
     if LOCALSTACK:
         logger.warn(f"Using localstack for {resource_type} resource")
-        return boto3.resource(resource_type, region_name=REGION, endpoint_url=LOCALSTACK_ADDRESS)
+        return boto3.resource(resource_type,
+                              region_name=REGION,
+                              endpoint_url=LOCALSTACK_ADDRESS,
+                              aws_access_key_id='foo',
+                              aws_secret_access_key='bar',
+                              )
     else:
         return boto3.resource(resource_type, REGION)
---
+++ app/settings.py
@@ -0,0 +1,21 @@
+import os
+
+
+def _get_boolean(env_name: str, fallback: str) -> bool:
+    return os.environ.get(env_name, fallback).lower() in ("true", "t", "1")
+
+
+DOWNLOAD_CHUNK_SIZE = int(os.environ.get("DOWNLOAD_CHUNK_SIZE", 2048))
+WORKING_FOLDER = os.environ.get("WORKING_FOLDER", "./work")
+REMOVE_WORK_DIR = _get_boolean("REMOVE_WORK_DIR", "True")
+RESCALE_ALTO = _get_boolean("RESCALE_ALTO", "True")
+MONITOR_SLEEP_SECS = float(os.environ.get('MONITOR_SLEEP_SECS', 30))
+
+# AWS
+REGION = os.environ.get('AWS_REGION', 'eu-west-1')
+INCOMING_QUEUE = os.environ.get('INCOMING_QUEUE')
+COMPLETED_TOPIC_ARN = os.environ.get('COMPLETED_TOPIC_ARN')
+
+# LocalStack
+LOCALSTACK = _get_boolean("LOCALSTACK", "False")
+LOCALSTACK_ADDRESS = os.environ.get("LOCALSTACK_ADDRESS", "http://localhost:4566")
